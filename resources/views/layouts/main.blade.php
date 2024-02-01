1c61626bed1ebf63657d23ab30d2297005b7bda2
Vanessa <vanessa35lv@gmail.com>
2024-02-01 16:40:22 -0300

    Criar - CRUD

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -48,7 +48,16 @@
             </nav>
         </header>
 
-        @yield('content')
+        <main>
+            <div class="container-fluid">
+                <div class="row">
+                    @if(session('msg'))
+                        <p class="msg">{{ session('msg') }}</p>
+                    @endif
+                    @yield('content')
+                </div>
+            </div>
+        </main>
 
         <footer>
             <p>HDC Events &copy; 2024</p>
